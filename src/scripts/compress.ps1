5e3461e1600182f56ae4f0c0ad1e8d0b27273914
Raji Rajagopalan <rajigopal@hotmail.com>
2022-01-30 17:59:11 -0800

    Adding scripts for Test Base

--- src/scripts/compress.ps1
+++ src/scripts/compress.ps1
@@ -1,6 +1,8 @@
 # Pass in as arguments:
 # param 1: root folder absolute path contain AppPackages, scripts and bin folders
 # param 2: name of the folder inside AppPackages that contains the msixbundle file for the app
+# This will create a zip file for Test Base upload including the needed msix files
+# installers and other scripts for install, launch, close, and uninstall
 
 $root = $args[0]
 $msixfolder = $root + "/AppPackages/" + $args[1]
